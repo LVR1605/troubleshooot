9120d2c58f01313cbe5712deafe13cef0bf729c4
lvr1605 <dolphinlvr1605@gmail.com>
2023-06-16 10:20:42 +0800

    1st

---
+++ client/components/UserList.vue
@@ -0,0 +1,38 @@
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      users: [],
+      currentPage: 1,
+      totalPages: null,
+    };
+  },
+  mounted() {
+    this.fetchUsers();
+  },
+  methods: {
+    fetchUsers() {
+      axios.get('/api/users', { params: { page: this.currentPage } })
+        .then(response => {
+          this.users = response.data.data;
+          this.currentPage = response.data.current_page;
+          this.totalPages = response.data.last_page;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    },
+  },
+};
+</script>
+<template>
+    <div>
+
+    </div>
+</template>
+
+<style scoped>
+
+</style>
